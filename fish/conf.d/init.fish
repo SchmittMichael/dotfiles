610210e0fab3c99c6296f7ca70dd6649b3489919
Michael Schmitt <leahcim.s@t-online.de>
2025-05-08 13:01:35 +0200

    updated confd/init file

--- fish/conf.d/init.fish
+++ fish/conf.d/init.fish
@@ -5,9 +5,13 @@ function __source
     end
 end
 
-__source zoxide init fish
+# loading .profile
+bass source ~/.profile
+
+# some sourcing
 __source fzf --fish
+__source zoxide init fish 
+functions -e __source
+
 
 set -g fish_key_bindings fish_vi_key_bindings
-
-functions -e __source
