2766705717b50a0acb5af6b3cf74421d43b15abe
Michael Schmitt <leahcim.s@t-online.de>
2025-06-17 11:20:50 +0200

    updated `update` function to support multiple distors

--- .config/fish/functions/custom/update.fish
+++ .config/fish/functions/custom/update.fish
@@ -1,8 +1,30 @@
-function update
-    sudo echo "Updating apt..."
+function __update_arch
+  echo "Updating pacman..."
+  sudo pacman -Syu
+
+  echo "Updating yay..."
+  yay
+end
+
+function __update_ubuntu
+    echo "Updating apt..."
     sudo apt update
     sudo apt upgrade -y
 
     echo "Updating snap..."
     sudo snap refresh
+end
+
+function update
+  set distro (sed -n "s/^ID=\(.*\)/\1/p" /etc/os-release)
+
+  switch $distro
+    case "arch"
+      __update_arch
+    case "ubuntu"
+      __update_ubuntu
+    case '*'
+      echo "Unknown distribution '$distro'"
+      exit 1
+  end
 end
