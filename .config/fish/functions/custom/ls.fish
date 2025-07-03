2bc483a4c97598b772dd85cfeaf4ad0560d29298
Michael Schmitt <leahcim.s@t-online.de>
2025-07-03 12:37:49 +0200

    fish fixex

--- .config/fish/functions/custom/ls.fish
+++ .config/fish/functions/custom/ls.fish
@@ -1,33 +1,33 @@
 function l
-    eza -la
+    eza -la $argv
 end
 
 function lss
-    eza -lf --color=always --sort=size
+    eza -lf --color=always --sort=size $argv
 end
 
 function ll
-    eza -l --group-directories-first
+    eza -l --group-directories-first $argv
 end
 
 function lda
-    eza -lDa
+    eza -lDa $argv
 end
 
 function lf
-    eza -lf --color=always
+    eza -lf --color=always $argv
 end
 
 
 function lfa
-    eza -lfa --color=always
+    eza -lfa --color=always $argv
 end
 
 function lh
-    eza -dl .* --group-directories-first
+    eza -dl .* --group-directories-first $argv
 
 end
 
 function lt
-    eza -al --sort=modified
+    eza -al --sort=modified $argv
 end
