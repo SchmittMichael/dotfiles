ae03ead936dc650d868a466b0291545f1d4bd16d
Michael Schmitt <michael.schmitt@anevis-solutions.com>
2025-10-06 11:57:13 +0200

    reworked vim-options.lua

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,13 +1,31 @@
-vim.cmd("filetype indent on")
+-- global options --
+vim.g.mapleader = " "
 
 -- default indentation settings
+vim.cmd("filetype indent on")
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-vim.g.mapleader = " "
+-- linenumbers
+vim.wo.relativenumber = true
+vim.wo.number = true
 
+-- split directions
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- set case insensitivity as default
+vim.opt.ic = true
+
+-- other
+vim.opt.langmenu = "en_US"
+vim.opt.undofile = true
+vim.opt.scrolloff = 8
+
+
+-- keymap options --
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
@@ -20,24 +38,13 @@ vim.keymap.set("n", "<leader>cs", ":nohlsearch<CR>")
 -- ctrl+backspace word delete in insert mode
 vim.keymap.set("i", "<C-H>", "<C-W>", { noremap = true })
 
--- linenumbers
-vim.wo.relativenumber = true
-vim.wo.number = true
-
--- scrolloff
-vim.opt.scrolloff = 8
-
--- split
-vim.cmd("set splitright")
-
 -- clipboard
 vim.opt.clipboard = "unnamed"
 vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true, desc = "Copy to system clipboard" })
 vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true, desc = "Copy selection to system clipboard" })
 
--- set case insensitivity as default
-vim.cmd("set ic")
 
+-- Custom commands --
 -- add buffer write + buffer delete command
 vim.api.nvim_create_user_command("Bx", ":bw", { desc = "Write and quit current buffer." })
 
@@ -53,10 +60,7 @@ end, { desc = "Enable completions." })
 -- enable spell checking
 vim.api.nvim_create_user_command("Spell", ":setlocal spell spelllang=en_us", { desc = "Enable spell cheking." })
 
--- set language to english on windows
--- vim.cmd("language en_US.UTF-8") -- disabled because of issues in wsl
-vim.cmd("set langmenu=en_US")
-
+-- Custom behavior --
 -- disable syntax tree features on large files
 TS_MAX_LINES = 3000
 
@@ -65,5 +69,3 @@ vim.diagnostic.config({
   virtual_text = true,
   severity_sort = true,
 })
-
-vim.opt.undofile = true
