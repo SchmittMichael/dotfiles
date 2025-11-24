ed4157f777afdaab44602aa08dc9ba6982aafbac
Michael Schmitt <leahcim.s@t-online.de>
2025-11-24 14:37:28 +0100

    improved `connect-to-laptop`

--- .config/fish/functions/custom/wsl.fish
+++ .config/fish/functions/custom/wsl.fish
@@ -6,17 +6,21 @@ set HOST_IDENTIFIER "$LAPTOP_USER@$LAPTOP_IP"
 function connect-to-laptop --description "Helper function to ssh to the WSL of my laptop, while disabling suspend for the time connected"
   set -l fish_exit_on_error true
 
+  if ! ssh -o BatchMode=yes -o ConnectTimeout=1 $HOST_IDENTIFIER "exit" 2>/dev/null
+    echo "Couldn't connect to laptop..."
+    return 1
+  end
+
+
   echo "Disabling auto suspend..."
-  __run_ps1_on_laptop "powercfg -change standby-timeout-dc 0"
-  __run_ps1_on_laptop "powercfg -change standby-timeout-ac 0"
+  __run_ps1_on_laptop "powercfg -change standby-timeout-dc 0; powercfg -change standby-timeout-ac 0"
 
   ssh "$HOST_IDENTIFIER"
 
   echo "Enabling auto suspend..."
-  __run_ps1_on_laptop "powercfg -change standby-timeout-dc 10"
-  __run_ps1_on_laptop "powercfg -change standby-timeout-ac 15"
+  __run_ps1_on_laptop "powercfg -change standby-timeout-dc 10; powercfg -change standby-timeout-ac 15"
 end
 
 function __run_ps1_on_laptop --argument-name command
-  ssh "$HOST_IDENTIFIER" "/mnt/c/Windows/System32/WindowsPowerShell/v1.0//powershell.exe  -c '$command'"
+  ssh "$HOST_IDENTIFIER" "/mnt/c/Windows/System32/WindowsPowerShell/v1.0//powershell.exe -c '$command'"
 end
