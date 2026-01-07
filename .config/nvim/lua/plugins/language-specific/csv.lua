b23c0cbf34f9601924857990b75f186a701bd9c5
Michael Schmitt <leahcim.s@t-online.de>
2026-01-07 11:26:55 +0100

    added new csv nvim plugin

--- .config/nvim/lua/plugins/language-specific/csv.lua
+++ .config/nvim/lua/plugins/language-specific/csv.lua
@@ -1,17 +1,35 @@
 return {
-  "cameron-wags/rainbow_csv.nvim",
-  config = function()
-    -- Workaround to fix lazy loading issues for plugin
-    require("rainbow_csv").setup()
-    require("rainbow_csv.fns").handle_buffer_enter()
-  end,
-  ft = {
-    "csv",
-    "tsv",
-    "csv_semicolon",
-    "csv_whitespace",
-    "csv_pipe",
-    "rfc_csv",
-    "rfc_semicolon",
+  {
+    "hat0uma/csvview.nvim",
+    opts = {
+      parser = { comments = { "#", "//" } },
+      keymaps = {
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      },
+    },
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+  },
+  {
+    -- Still using for automatic CSV dialect detection
+    "cameron-wags/rainbow_csv.nvim",
+    config = function()
+      -- Workaround to fix lazy loading issues for plugin
+      require("rainbow_csv").setup()
+      require("rainbow_csv.fns").handle_buffer_enter()
+    end,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
   },
 }
