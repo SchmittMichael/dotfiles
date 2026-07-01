b6921ad1b950ad95ebd38b3b398730749c97e9fa
Michael Schmitt <leahcim.s@t-online.de>
2026-07-01 15:54:50 +0200

    [FISH]: improved startup time

--- .config/fish/conf.d/abbreviation.fish
+++ .config/fish/conf.d/abbreviation.fish
@@ -1,6 +1,22 @@
 # shortcuts
-abbr -a off 'poweroff'
-abbr -a py "python3"
+abbr -a off   'poweroff'
+abbr -a py    'python3'
+abbr -a d     'docker'
+abbr -a n     'nvim'
+## git abbr
+abbr -a g     'git'
+abbr -a gs    'git stash'
+abbr -a gsp   'git stash pop' 
+abbr -a gl    'git pull'
+abbr -a ga    'git add'
+abbr -a gcm   'git commit -m'
+abbr -a gp    'git push'
+abbr -a gcom  'git checkout origin/master'
+abbr -a gco   'git checkout'
+abbr -a gd    'git diff'
+abbr -a grs   'git restore'
+abbr -a gst   'git status'
+
 # typo fixes
 abbr -a suod 'sudo'
 abbr -a nivm 'nvim'
--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -1,10 +1,23 @@
+function add_to_path --description "Prepends a directory to PATH if it exists"
+  set -l dir "$argv[1]"
+
+  if test -d "$dir"
+    set -gx PATH "$dir" $PATH
+  end
+end
+
 if not type -q fisher
   echo "[WARNING]: Detected that 'fisher' isn't installed. Assuming fish isn't properly set up. Run 'fisher-init' to complete setup."
 end
 
-# loading .profile
-if type -q bass
+
+if test -f "$HOME/.path_additions"
+  for path in (string collect < "$HOME/.path_additions")
+    add_to_path $path
+  end
+else if type -q bass
+  echo "[WARNING]: using .profile fallback instead of '.path_additions'"
   bass source "$HOME/.profile"
 else
-  echo "[ERROR]: Couldn't load $HOME/.profile, 'bass' isn't installed..."
+  echo "[ERROR]: Couldn't load additional path variables"
 end
