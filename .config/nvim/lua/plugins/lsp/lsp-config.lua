89c7e766ac92f5d50837ebc9b717ca5a9fcf3d1b
Michael Schmitt <leahcim.s@t-online.de>
2026-07-10 10:52:12 +0200

    [NVIM]: tried reimplemting LspInfo and LspRestart

--- .config/nvim/lua/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -1,7 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
-  cmd = { "LspInfo", "LspRestart", "LspInstall", "LspUninstall" },
+  cmd = { "LspInfo", "LspRestart" },
   config = function()
     vim.lsp.inlay_hint.enable(true)
 
@@ -12,6 +12,21 @@ return {
         formatterMode = "typstyle",
       },
     }
+
+    vim.api.nvim_create_user_command(
+      "LspInfo",
+      function() vim.cmd(":checkhealth vim.lsp  ") end,
+      { desc = "Informations about active lsp clients." }
+    )
+
+    -- vim.api.nvim_create_user_command("LspRestart", function()
+    --   local clients = vim.lsp.get_clients()
+    --   for _, client in ipairs(clients) do
+    --     client.restart()
+    --     vim.print(vim.inspect(vim.lsp))
+    --     vim.print(vim.inspect(client))
+    --   end
+    -- end, { desc = "List active lsp clients." })
   end,
   keys = {
     { "<C-s>", vim.lsp.buf.signature_help, mode = { "n", "i" }, desc = "Show function signature" },
