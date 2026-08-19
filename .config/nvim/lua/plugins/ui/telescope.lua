a38eafcd5819198fd3020424aa87bab7c9f64d30
Michael Schmitt <leahcim.s@t-online.de>
2026-08-19 18:13:59 +0200

    [NVIM]: added telescope memory

--- .config/nvim/lua/plugins/ui/telescope.lua
+++ .config/nvim/lua/plugins/ui/telescope.lua
@@ -1,5 +1,65 @@
+local queries = {}
+
+local function telescope_persistence(name, opts, persistence_key)
+  local builtin = require("telescope.builtin")
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+
+  opts = vim.deepcopy(opts or {})
+
+  persistence_key = persistence_key or name
+
+  local previous = queries[persistence_key] or ""
+  opts.default_text = previous
+
+  local original_attach = opts.attach_mappings
+
+  opts.attach_mappings = function(prompt_bufnr, map)
+    vim.api.nvim_create_autocmd("BufLeave", {
+      buffer = prompt_bufnr,
+      once = true,
+      callback = function() queries[persistence_key] = action_state.get_current_line() end,
+    })
+
+    -- escape in visual mode instantly
+    vim.keymap.set("v", "<esc>", function() actions.close(prompt_bufnr) end, {
+      buffer = prompt_bufnr,
+      nowait = true,
+    })
+
+    -- If there was a previous query, select it.
+    if previous ~= "" then
+      vim.schedule(function()
+        if vim.api.nvim_get_current_buf() == prompt_bufnr then
+          local picker = action_state.get_current_picker(prompt_bufnr)
+          local prompt_prefix = picker.prompt_prefix or "> "
+
+          vim.cmd("stopinsert")
+
+          -- Move to the first character after the Telescope prompt prefix.
+          vim.api.nvim_win_set_cursor(0, { 1, #prompt_prefix })
+          vim.cmd("normal! v$")
+        end
+      end)
+    end
+
+    if original_attach then
+      return original_attach(prompt_bufnr, map)
+    end
+
+    return true
+  end
+
+  builtin[name](opts)
+end
+
+local function persistent_picker(name, opts, persistence_key)
+  return function() telescope_persistence(name, opts, persistence_key) end
+end
+
 return {
   "nvim-telescope/telescope.nvim",
+
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
@@ -8,44 +68,125 @@ return {
   cmd = "Telescope",
   keys = {
     -- LSP related
-    { "<leader>gd", ":Telescope lsp_definitions<CR>", mode = { "n" }, desc = "Show lsp definitions" },
-    { "<leader>gr", ":Telescope lsp_references<CR>", mode = { "n" }, desc = "Show lsp references" },
-    { "<leader>ca", vim.lsp.buf.code_action, mode = { "n" }, desc = "Show code actions" },
+    {
+      "<leader>gd",
+      persistent_picker("lsp_definitions"),
+      mode = "n",
+      desc = "Show lsp definitions",
+    },
+    {
+      "<leader>gr",
+      persistent_picker("lsp_references"),
+      mode = "n",
+      desc = "Show lsp references",
+    },
+
+    -- Not Telescope
+    {
+      "<leader>ca",
+      vim.lsp.buf.code_action,
+      mode = "n",
+      desc = "Show code actions",
+    },
+
     -- Often used
     {
       "<leader>fg",
-      function()
-        require("telescope.builtin").grep_string({
-          shorten_path = true,
-          word_match = "-w",
-          only_sort_text = true,
-          search = "",
-        })
-      end,
+      persistent_picker("grep_string", {
+        shorten_path = true,
+        word_match = "-w",
+        only_sort_text = true,
+        search = "",
+      }, "grep_words"),
+      mode = "n",
       desc = "Grepping in files",
     },
-    { "<leader>ff", ":Telescope find_files<CR>", mode = { "n" }, desc = "Find in files" },
+
+    {
+      "<leader>ff",
+      persistent_picker("find_files", nil, "find_files"),
+      mode = "n",
+      desc = "Find in files",
+    },
+
     {
       "<leader>fF",
-      function() require("telescope.builtin").find_files({ hidden = true }) end,
-      mode = { "n" },
+      persistent_picker("find_files", {
+        hidden = true,
+      }, "find_all_files"),
+      mode = "n",
       desc = "Find in all files",
     },
-    { "<leader>:", ":Telescope command_history<CR>", mode = { "n" }, desc = "View command history" },
-    { "<leader>fs", ":Telescope grep_string<CR>", mode = { "n", "v" }, desc = "Find string in files" },
+
+    {
+      "<leader>:",
+      persistent_picker("command_history"),
+      mode = "n",
+      desc = "View command history",
+    },
+
+    {
+      "<leader>fs",
+      persistent_picker("grep_string", nil, "find_string"),
+      mode = { "n", "v" },
+      desc = "Find string in files",
+    },
+
     -- Telescope prefix
-    { "<leader>tb", ":Telescope buffers<CR>", desc = "Show active buffers" },
-    { "<leader>tr", ":Telescope resume<CR>", mode = { "n" }, desc = "Resume telescope" },
-    { "<leader>tk", ":Telescope keymaps<CR>", mode = { "n" }, desc = "Show all keymaps" },
-    { "<leader>tk", ":Telescope oldfiles<CR>", mode = { "n" }, desc = "Show recent files" },
-    { "<leader>tgc", ":Telescope git_commits<CR>", mode = { "n" }, desc = "Show git commits" },
-    { "<leader>tgb", ":Telescope git_branches<CR>", mode = { "n" }, desc = "Show git branches" },
-    { "<leader>tgs", ":Telescope git_status<CR>", mode = { "n" }, desc = "Show git status" },
+    {
+      "<leader>tb",
+      persistent_picker("buffers"),
+      mode = "n",
+      desc = "Show active buffers",
+    },
+
+    {
+      "<leader>tr",
+      function() require("telescope.builtin").resume() end,
+      mode = "n",
+      desc = "Resume telescope",
+    },
+
+    {
+      "<leader>tk",
+      persistent_picker("keymaps"),
+      mode = "n",
+      desc = "Show all keymaps",
+    },
+
+    {
+      "<leader>to",
+      persistent_picker("oldfiles"),
+      mode = "n",
+      desc = "Show recent files",
+    },
+
+    {
+      "<leader>tgc",
+      persistent_picker("git_commits"),
+      mode = "n",
+      desc = "Show git commits",
+    },
+
+    {
+      "<leader>tgb",
+      persistent_picker("git_branches"),
+      mode = "n",
+      desc = "Show git branches",
+    },
+
+    {
+      "<leader>tgs",
+      persistent_picker("git_status"),
+      mode = "n",
+      desc = "Show git status",
+    },
   },
 
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+
     telescope.setup({
       extensions = {
         ["ui-select"] = {
