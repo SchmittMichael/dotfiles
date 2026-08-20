e7a57eec756ddc7b543fa060a65659959070a3c7
Michael Schmitt <leahcim.s@t-online.de>
2026-08-20 19:37:26 +0200

    [NVIM]: fixed which-key popping at when grepping files

--- .config/nvim/lua/plugins/ui/which-key.lua
+++ .config/nvim/lua/plugins/ui/which-key.lua
@@ -6,8 +6,10 @@ return {
     vim.o.timeoutlen = 300
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    disable = {
+      ft = {
+        "TelescopePrompt"
+      }
+    }
   },
 }
